0100938f91604739e34a6c8c0bb1dfd45791965c
Declan Cook <DeclanCook44@gmail.com>
2014-03-15 14:18:44 +0000

    Impement basic dragging of tabs and covert to jquery plugin

--- chrometab.js
+++ chrometab.js
@@ -1,13 +1,45 @@
-(function($){
+;(function($){
+    $.fn.extend({
+        chrometab: function(options) {
 
-    $('.tab').on('click', function(){
+            this.defaultOptions = {};
+            var settings = $.extend({}, this.defaultOptions, options);
 
-        var $this = $(this),
-            $pane = $this.data('target');
+            return this.each(function() {
+                var $tabContainer = $(this),
+                    $tabs = $tabContainer.find('.tab'),
+                    mouseDown = false,
+                    draggedTab = null;
 
-        $('.tab').add('.tab-pane').removeClass('active')
-        $this.add($pane).addClass('active')
+                $tabs.on('mousedown', function(e){
+                    var $this = $(this),
+                        $pane = $this.data('target');
 
-    })
+                    $('.tab').add('.tab-pane').removeClass('active')
+                    $this.add($pane).addClass('active')
 
-})(jQuery)
+                    mouseDown = true;
+                    draggedTab = $this;
+                    offset = e.offsetX
+                })
+
+                $(document).on('mousemove', function(e){
+                    if(!mouseDown && !draggedTab) return;
+
+                    var $this = $(this);
+
+                    draggedTab.offset({ left: e.clientX - offset })
+                    e.preventDefault()
+                })
+
+                $(document).on('mouseup', function(){
+                    if(mouseDown && draggedTab){
+
+                    }
+                    mouseDown = false;
+                    draggedTab = null;
+                })
+            });
+        }
+    });
+})(jQuery);
