808f99f85cdac077d1bb299b47d7de427b3aba7b
Declan Cook <DeclanCook44@gmail.com>
2014-03-15 13:30:30 +0000

    Add tab-pane markup and functionalilty

--- chrometab.js
+++ chrometab.js
@@ -1,5 +1,13 @@
 (function($){
 
+    $('.tab').on('click', function(){
 
+        var $this = $(this),
+            $pane = $this.data('target');
+
+        $('.tab').add('.tab-pane').removeClass('active')
+        $this.add($pane).addClass('active')
+
+    })
 
 })(jQuery)
